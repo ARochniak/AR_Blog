6d873d9412525dfa02e29340f62d849d86fb747f
Anton Rochniak <rochniak15@gmail.com>
2020-08-10 01:01:40 +0300

    Update app ptops type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import { Provider } from 'react-redux';
+import { AppProps } from 'next/app'
 import { useStore } from '../src/store/store';
 import '../public/style.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   const store = useStore({ posts: pageProps.posts });
 
   return (
